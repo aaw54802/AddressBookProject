5dad3eed578cd4646c3463ff896c95a3a2e000b5
aaw54802 <aaw54802@email.vccs.edu>
2024-09-20 02:41:29 -0400

    Add project files.

---
+++ AddressBookProject.cpp
@@ -0,0 +1,13 @@
+// AddressBookProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//master to address
+//address to date
+//date to person(person and extperson)
+//person to addressBook-application
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
